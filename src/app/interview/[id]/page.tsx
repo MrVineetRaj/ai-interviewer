694a3928aea2fb21e109ec8e530d40ab4cfff86c
mrvineetraj <vineetrajrj26@gmail.com>
2025-09-07 01:47:35 +0530

    speech to text with intelligent recorder is working properly

---
+++ src/app/interview/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { InterviewPage } from "@/components/interview-page";
+import React from "react";
+
+const Page = async () => {
+  return (
+    <div className="flex h-screen max-h-screen w-full max-w-screen bg-background p-4">
+      <InterviewPage />
+    </div>
+  );
+};
+
+export default Page;
